7b073dddd8df9e1d4c144bd4b42faca8b0ffbbec
dominic wyler <dominic.wyler@gmail.com>
2015-05-27 23:23:39 +0200

    added test case for a basic game

--- Trivia.Tests/GameTest.cs
+++ Trivia.Tests/GameTest.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System.IO;
+using NUnit.Framework;
+using UglyTrivia;
 
 namespace Trivia.Tests
 {
@@ -6,10 +8,16 @@ namespace Trivia.Tests
     public class GameTest
     {
         [Test]
-        public void UNIT_SCENARIO_RESULT()
-        {
-            
-        }
+        public void Test1() {
+        var outputStringWriter = new StringWriter { NewLine = "\n" };
+        Game aGame = new Game(outputStringWriter);
+        aGame.add("Chet");
+        aGame.wrongAnswer();
+        Assert.AreEqual(outputStringWriter.ToString(), "Chet was added\n" +
+                "They are player number 1\n" +
+                "Question was incorrectly answered\n" +
+                "Chet was sent to the penalty box\n");
+    }
     }
 }
 
