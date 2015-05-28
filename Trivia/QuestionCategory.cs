4c1a37b2602002b249a68ac2958fbfc8ee0436fb
dominic wyler <dominic.wyler@gmail.com>
2015-05-28 10:33:57 +0200

    removed unused class QuestionCategory

--- Trivia/QuestionCategory.cs
+++ Trivia/QuestionCategory.cs
@@ -1,13 +1,5 @@
 namespace UglyTrivia
 {
-    public class QuestionCategory
-    {
-        public const string CATEGORY_POP = "Pop";
-        public const string CATEGORY_SCIENCE = "Science";
-        public const string CATEGORY_SPORTS = "Sports";
-        public const string CATEGORY_ROCK = "Rock";
-    }
-
     public enum QuestionCategory2
     {
         Pop,
